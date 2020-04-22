0b272dc45bd90c28c2a96286afa5e394b8dcac8a
Atsushi Neki <atsushin@fast.au.fujitsu.com>
2020-04-23 00:27:09 +1000

    Update fabric-test branch to release-2.1

--- e2e-test/runTestSuite.sh
+++ e2e-test/runTestSuite.sh
@@ -8,24 +8,23 @@ mkdir -p $GOPATH/src/github.com/hyperledger
 
 pushd $GOPATH/src/github.com/hyperledger
 if [ ! -d fabric-test ]; then
-  git clone https://github.com/hyperledger/fabric-test.git -b release-2.0
+  git clone https://github.com/hyperledger/fabric-test.git -b release-2.1
 fi
 cd fabric-test
-git checkout 78b2c83c4e194ab475789b39378d4f5ddbead511
+git checkout 85bfeb408d589a543cd71e4c7e692a572a6b85f5
 echo "#### Updated each sub-module under fabric-test repo"
 popd
 
 pushd $GOPATH/src/github.com/hyperledger/fabric-test/tools/PTE
 rm -rf node_modules package-lock.json
-npm install fabric-client@1.4.8
-npm install fabric-ca-client@1.4.8
+npm install
 echo "#### Installed required node packages"
 popd
 
 rm -f PTE
 ln -s $GOPATH/src/github.com/hyperledger/fabric-test/tools/PTE ./PTE
 
-pushd specs
+pushd specs2
 echo "#### Starting Ginkgo based test suite"
 ginkgo -v -stream
 popd
